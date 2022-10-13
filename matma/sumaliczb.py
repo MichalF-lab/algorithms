b7c823f8f7e34036f7a54517cd1e14aa8ba35e79
MichalF-lab <micha-lfrackowiak@wp.pl>
2022-10-13 10:42:05 +0200

    commit to master

--- matma/sumaliczb.py
+++ matma/sumaliczb.py
@@ -26,7 +26,22 @@ print(sumaFor(5))
 def sumaRek(inputt):
     suma = 0
     if(inputt != 0):
-        suma += inputt + sumaRek(inputt - 1)
+        suma = inputt + sumaRek(inputt - 1)
     return suma
 
 print(sumaRek(5))
+
+def cFibbonaci(inputt,temp1 = 0,temp2 = 1):
+    if(inputt == 0):
+        return 0
+    if(inputt == 1):
+        return temp2
+    temp3 = temp2 + temp1
+    return cFibbonaci(inputt - 1,temp2,temp3)
+
+print(cFibbonaci(40))
+
+
+suma = 0
+
+print( suma)
